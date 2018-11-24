7be548850073d0685b40bf6d5363b04cdace650c
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-11-23 18:37:34 -0600

    Make omega parameter explicit, add cmd args, fixes

--- Python/BracketsPowerModel/Likelihood/analytical/toy.py
+++ Python/BracketsPowerModel/Likelihood/analytical/toy.py
@@ -14,8 +14,7 @@ from sympy.matrices import hessian
 import numpy as np
 import sympy as sp
 
-
-truth = {
+reference_truth = {
     2016: [
         1, 0, 1, 0, 0, 1, 1, 1,
         1, 1, 0, 1, 0, 1, 0, 1,
@@ -35,8 +34,6 @@ truth = {
         1, 1, 1, 1, 0, 1, 1, 1
     ]
 }
-
-reference_truth = deepcopy(truth)
 #     7/8, 0.5, 7/8, 6/8, 3/8, 1., 6/8, 1.
 
 
@@ -49,19 +46,8 @@ p = [
     0.9926470588235294, 0.5, 0.6544117647058824, 0.7941176470588235, 0.625, 0.8455882352941176, 0.6176470588235294, 0.9411764705882353
 ]
 
-omega = np.array([
-    0.9926470588235294,
-    0.5,
-    0.6544117647058824,
-    0.7941176470588235,
-    0.625,
-    0.8455882352941176,
-    0.6176470588235294,
-    0.9411764705882353
-])
 
-
-def _full_form_objective_function_32(year, break_region=False):
+def _full_form_objective_function_32(year, break_region=False, truth=None):
     all_factors = []
     if break_region:
         ctrl = 4
@@ -81,7 +67,7 @@ def _full_form_objective_function_32(year, break_region=False):
     return '*'.join(all_factors)
 
 
-def _full_form_objective_function_31(year, break_region=False):
+def _full_form_objective_function_31(year, break_region=False, truth=None):
     all_summands = []
     if break_region:
         ctrl = 4
@@ -133,23 +119,23 @@ objective_fns = {
 }
 
 
-def _full_form_objective_function(year, alpha, break_region):
-    return objective_fns[str(alpha)](year, break_region)
+def _full_form_objective_function(year, alpha, break_region, truth):
+    return objective_fns[str(alpha)](year, break_region, truth)
 
 
-def full_form_objective_fn(alpha=31, break_region=False):
-    year_objs = [_full_form_objective_function(year, alpha, break_region) for year in truth.keys()]
+def full_form_objective_fn(alpha=31, break_region=False, truth=None):
+    year_objs = [_full_form_objective_function(year, alpha, break_region, truth) for year in truth.keys()]
     return '*'.join(year_objs)
 
 
-def get_subs(symbols, probs=omega):
+def get_subs(symbols, probs):
     return [
         (symbols['p_{}'.format(i)], probs[i])
         for i in range(n_games)
     ]
 
 
-def mle_probs():
+def mle_probs(truth):
     data = [np.array(bits).reshape(4, -1) for _, bits in truth.items()]
     data = np.vstack(data)
     return data.mean(axis=0)
@@ -162,23 +148,58 @@ def select_columns(data, cols):
     return new_data
 
 
+def test_newtons_method():
+    # https://jmahaffy.sdsu.edu/courses/f00/math122/lectures/newtons_method/newtonmethodeg.html
+    symbols = {'x': sp.Symbol('x')}
+    obj = parse_expr('4 + 8*(x**2) - x**4', symbols)
+    H = hessian(obj, list(symbols.values()))
+    grad = [sp.diff(obj, p_i) for p_i in symbols.values()]
+    omega = 1.1
+
+    def get_subs():
+        return [(symbols['x'], omega)]
+
+    gamma = 0.5
+
+    while True:
+        H_eval = np.array([d2f.subs(get_subs()) for d2f in H],
+                          dtype=float).reshape(len(symbols.items()), len(symbols.items()))
+        grad_eval = np.array(
+            [f_prime.subs(get_subs()) for f_prime in grad], dtype=float)
+        omega_next = omega - gamma * np.linalg.inv(H_eval).dot(grad_eval)
+        if np.linalg.norm(omega - omega_next) < 1e-20:
+            break
+        print('omega ', omega_next)
+        omega = omega_next
+    import pdb; pdb.set_trace()
+
+
 if __name__ == '__main__':
-    print('Usage: python toy.py <gamma>')
+    print('Usage: python toy.py <gamma> <output_dir>')
     import json
+    import os
     import sys
     from itertools import permutations
 
     gamma = float(sys.argv[1])
+    if len(sys.argv) > 2:
+        output_dir = sys.argv[2]
+    else:
+        output_dir = 'Likelihood/analytical/logs'
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     for config in permutations(range(8), n_games):
-        out = open('Likelihood/analytical/logs/out.{}-{}.txt'.format('_'.join([str(x) for x in config]), gamma), 'w')
-        truth = select_columns(reference_truth, config)
-        input_data = json.dumps(truth, indent=4)
+        out = open('{}/out.{}-{}.txt'.format(output_dir, '_'.join([str(x) for x in config]), gamma), 'w')
+        gt_data = select_columns(reference_truth, config)
+        input_data = json.dumps(gt_data)
         # import pdb; pdb.set_trace()
         out.write(input_data)
         out.write('\n')
 
-        omega = np.random.rand(8)
+        # omega = np.random.rand(n_games)
+        omega = mle_probs(gt_data)
         # print('Objective: ', full_form_objective_fn(break_region=True))
         omega_symbols = {
             'p_0': sp.Symbol('p_0'),
@@ -194,9 +215,11 @@ if __name__ == '__main__':
         symbols = {k: v
                    for k, v in omega_symbols.items()
                    if int(k.split('_')[1]) < n_games}
-        objective_31 = parse_expr(full_form_objective_fn(break_region=True),
+        objective_31 = parse_expr(full_form_objective_fn(break_region=True, truth=gt_data),
                                   local_dict=symbols)
         print('Objective: ', objective_31)
+        print('MLE probs: ', mle_probs(gt_data))
+        print('P(M_y >= {} | MLE) = {}'.format(n_games - 1, objective_31.subs(get_subs(symbols, mle_probs(gt_data)))))
         # import pdb; pdb.set_trace()
         H = hessian(objective_31, list(symbols.values()))
         grad = [sp.diff(objective_31, p_i) for p_i in symbols.values()]
@@ -205,53 +228,64 @@ if __name__ == '__main__':
         iter_count = 0
         max_iters = 10000
         history = []
-        omega = omega[:n_games]
-        probs = []
+        probs = [0]
+        clipped_counter = 0
+        clipped_dim_counter = 0
         while True:
             history.append(omega)
-            H_eval = np.array([d2f.subs(get_subs(symbols)) for d2f in H], dtype=float).reshape(n_games, n_games)
-            grad_eval = np.array([f_prime.subs(get_subs(symbols)) for f_prime in grad], dtype=float)
-            omega_next = np.clip(omega + gamma * np.linalg.inv(H_eval).dot(grad_eval), 0., 1.)
+            H_eval = np.array([d2f.subs(get_subs(symbols, omega)) for d2f in H], dtype=float).reshape(n_games, n_games)
+            grad_eval = np.array([f_prime.subs(get_subs(symbols, omega)) for f_prime in grad], dtype=float)
+            try:
+                raw_omega = omega + gamma * np.linalg.inv(H_eval).dot(grad_eval)
+            except np.linalg.LinAlgError:
+                raw_omega = omega + gamma * np.linalg.pinv(H_eval).dot(grad_eval)
+            omega_next = np.clip(raw_omega, 0., 1.)
 
-            print('Gradient: ', grad_eval)
+            # print('Gradient: ', grad_eval)
             out.write('Gradient: ' + str(grad_eval))
             out.write('\n')
 
-            print('New omega:', omega.tolist(), ' - L2 diff: ', np.linalg.norm(omega - omega_next))
+            p = objective_31.subs(get_subs(symbols, omega))
+            print('P(M_y >= {}) = {}. '.format(n_games - 1, p), 'New omega:', omega.tolist(), ' - L2 diff: ', np.linalg.norm(omega - omega_next))
+            out.write('P(M_y >= {}) = {}. '.format(n_games - 1, p))
             out.write('New omega: [')
             out.write(', '.join(omega.astype(str).tolist()))
             out.write('] - L2 diff: ')
             out.write(str(np.linalg.norm(omega - omega_next)))
             out.write('\n')
 
-            p = objective_31.subs(get_subs(symbols))
-            print('P(M_y >= {}) = {}'.format(n_games - 1, p))
-            out.write('P(M_y >= {}) = {}'.format(n_games - 1, p))
-            out.write('\n')
-
             probs.append(p)
 
             if iter_count > max_iters:
                 break
-            if np.linalg.norm(omega - omega_next) < 1e-20:
+            if np.linalg.norm(omega_next - omega) < 1e-15:
                 break
             omega = omega_next
             iter_count += 1
+            clipped_counter += 1 if np.any((raw_omega < 0) | (raw_omega > 1.)) else 0
+            clipped_dim_counter += np.count_nonzero((raw_omega < 0) | (raw_omega > 1.))
+
+        print('# iterations', iter_count)
+        print('# of times omega was truncated', clipped_counter)
+        print('# of times p_i was truncated', clipped_dim_counter)
+        out.write('# iterations {}\n'.format(iter_count))
+        out.write('# of times omega was truncated {}\n'.format(clipped_counter))
+        out.write('# of times p_i was truncated {}\n'.format(clipped_dim_counter))
 
         print('Max P(.) = ', np.max(probs))
         out.write('Max P(.) = ' + str(np.max(probs)))
         out.write('\n')
 
-        print('Final Omega: ', get_subs(symbols))
-        out.write('Final Omega: ' + str(get_subs(symbols)))
+        print('Final Omega: ', get_subs(symbols, omega))
+        out.write('Final Omega: ' + str(get_subs(symbols, omega)))
         out.write('\n')
 
-        print('MLE probs: ', get_subs(symbols, mle_probs()))
-        out.write('MLE probs:   ' + str(get_subs(symbols, mle_probs())))
+        print('MLE probs: ', get_subs(symbols, mle_probs(gt_data)))
+        out.write('MLE probs:   ' + str(get_subs(symbols, mle_probs(gt_data))))
         out.write('\n')
 
-        print('P(M_y >= {} | MLE) = {}'.format(n_games - 1, objective_31.subs(get_subs(symbols, mle_probs()))))
-        out.write('P(M_y >= {} | MLE) = {}'.format(n_games - 1, objective_31.subs(get_subs(symbols, mle_probs()))))
+        print('P(M_y >= {} | MLE) = {}'.format(n_games - 1, objective_31.subs(get_subs(symbols, mle_probs(gt_data)))))
+        out.write('P(M_y >= {} | MLE) = {}'.format(n_games - 1, objective_31.subs(get_subs(symbols, mle_probs(gt_data)))))
         out.write('\n')
 
         # import pdb; pdb.set_trace()
