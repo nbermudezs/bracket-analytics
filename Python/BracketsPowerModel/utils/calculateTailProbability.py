7e47d0490b4c9fe2c36a5b797ef1944d7a8c5969
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2019-01-30 17:37:50 -0600

    Add util to calcualte probability of score given indv. model dist

---
+++ Python/BracketsPowerModel/utils/calculateTailProbability.py
@@ -0,0 +1,49 @@
+__author__ = "Nestor Bermudez"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__email__ = "nab6@illinois.edu"
+__status__ = "Development"
+
+
+import json
+import numpy as np
+
+
+def calculate_tail_p(score, num_trials, year, batch_number, models_path, summary_path):
+    with open(models_path) as f:
+        models = json.load(f)
+    n_models = len(models['models'])
+    p = []
+    for model in models['models']:
+        name = model['modelName']
+        if model.get('ref') is not None:
+            summary_path = 'Summary_{}_models'.format(model['ref'])
+        with open('{}/stats/{}/{}_batch{}.json'.format(summary_path, year, name, batch_number)) as f:
+            stats = json.load(f)
+        percentiles = [0] + stats['percentiles'][1:] + [stats['max'], 1920]
+        for i in range(100, -1, -1):
+            if score > percentiles[i] and score <= percentiles[i+1]:
+                p.append((100 - i) / 100.)
+                break
+    p_all = 1. / n_models * np.sum(p)
+    return p_all
+
+
+if __name__ == '__main__':
+    import os
+    import sys
+
+    num_trials = int(sys.argv[1])
+    num_batches = int(sys.argv[2])
+    models_path = sys.argv[3]
+    summary_path = sys.argv[4]
+    score = int(sys.argv[5])
+
+    if not os.path.exists('{}/stats'.format(summary_path)):
+        print('model stats must be calculated before')
+        exit(1)
+
+    for year in range(2013, 2019):
+        for batch_number in range(num_batches):
+            p = calculate_tail_p(score, num_trials, year, batch_number, models_path, summary_path)
+            print(year, batch_number, p)
