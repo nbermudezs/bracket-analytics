a538f277b460c98d3c15898e06286cbecdcd303d
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2019-02-18 18:26:34 -0600

    Add script for best triplet configurations

---
+++ Python/BracketsPowerModel/triplets-improvement-4plus.sh
@@ -0,0 +1,16 @@
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 1
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 21
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 29
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 33
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 71
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 86
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 92
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 105
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 106
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 109
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 113
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 117
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 119
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 123
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 126
+
