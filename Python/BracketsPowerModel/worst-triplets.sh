b34a0404e5d4abb35654f19a3964095a1cd1a474
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2019-02-20 12:56:28 -0600

    Add script to generate brackets with bad triplets

---
+++ Python/BracketsPowerModel/worst-triplets.sh
@@ -0,0 +1,36 @@
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 3
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 4
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 5
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 10
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 11
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 12
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 13
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 20
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 22
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 23
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 31
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 35
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 38
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 45
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 60
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 65
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 73
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 74
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 93
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 96
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 99
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 122
+
+# removing S16_E8_2
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 0
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 2
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 7
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 8
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 9
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 28
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 33
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 53
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 57
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 63
+python bitwise_triplet_generator.py 50000 50 Ensemble/models/all_triplet_comb_models.json 102
+
