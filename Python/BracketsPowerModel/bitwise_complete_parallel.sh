42c00c6209ef821ace851f0a473945a3778d3e7e
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2019-03-09 14:52:01 -0600

    Add files for triplets and paths involving champions (region or national)

---
+++ Python/BracketsPowerModel/bitwise_complete_parallel.sh
@@ -0,0 +1,12 @@
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 0
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 1
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 2
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 3
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 4
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 5
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 6
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 7
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 8
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 9
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 10
+python bitwise_complete_generator.py 50000 10 Ensemble/models/bitwise_complete.json 11
